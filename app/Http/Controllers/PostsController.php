1a827f6b445c89e69ca0e61ec9653f1a16228c9b
Mauro Nunes <mauraoemau@hotmail.com>
2018-05-02 09:56:33 -0300

    Controllers posts

---
+++ app/Http/Controllers/PostsController.php
@@ -0,0 +1,23 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: mauronunes
+ * Date: 05/04/2018
+ * Time: 10:38
+ */
+
+namespace App\Http\Controllers;
+
+use App\Post;
+
+class PostsController extends Controller
+{
+    public function index()
+    {
+        $posts = Post::where('id', 1)
+            ->get()
+            ->comments();
+
+        dd($posts->toArray());
+    }
+}
